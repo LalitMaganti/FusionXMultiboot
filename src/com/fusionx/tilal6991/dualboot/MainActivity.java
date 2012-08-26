1687090d0bfb11ce6a59fbd37ab7fa6ca2aadece
Lalit Maganti <lalitmaganti@gmail.com>
2012-08-26 12:08:28 +0100

    Fix tabs

--- src/com/fusionx/tilal6991/dualboot/MainActivity.java
+++ src/com/fusionx/tilal6991/dualboot/MainActivity.java
@@ -15,58 +15,58 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		LinearLayout ll =s (LinearLayout) findViewById(R.id.layout);
+        LinearLayout ll = (LinearLayout) findViewById(R.id.layout);
 
-		final String multibootdir = Environment.getExternalStorageDirectory()
-				.getAbsolutePath() + "/multiboot/";
+        final String multibootdir = Environment.getExternalStorageDirectory()
+                .getAbsolutePath() + "/multiboot/";
 
-		File root = new File(multibootdir);
-		final String files[] = root.list(audioFilter);
+        File root = new File(multibootdir);
+        final String files[] = root.list(audioFilter);
 
-		runRootCommands("chmod 777 " + multibootdir + "*.sh");
+        runRootCommands("chmod 777 " + multibootdir + "*.sh");
 
-		for (final String file : files) {
-			Button btn = new Button(this);
-			btn.setText(file);
-			ll.addView(btn);
-			btn.setOnClickListener(new View.OnClickListener() {
-				public void onClick(View v) {
-					DisplayToast("Rebooting into specified ROM");
-					runRootCommands("sh " + multibootdir + file);
-				}
-			});
-		}
-	}
+        for (final String file : files) {
+            Button btn = new Button(this);
+            btn.setText(file);
+            ll.addView(btn);
+            btn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    DisplayToast("Rebooting into specified ROM");
+                    runRootCommands("sh " + multibootdir + file);
+                }
+            });
+        }
+    }
 
-	FilenameFilter audioFilter = new FilenameFilter() {
-		public boolean accept(File dir, String name) {
-			if (name.endsWith(".sh"))
-				return true;
-			else
-				return false;
-		}
-	};
+    FilenameFilter audioFilter = new FilenameFilter() {
+        public boolean accept(File dir, String name) {
+            if (name.endsWith(".sh"))
+                return true;
+            else
+                return false;
+        }
+    };
 
-	public void runRootCommands(String cmd) {
-		Process p = null;
-		try {
-			p = Runtime.getRuntime().exec("su");
-			DataOutputStream os = new DataOutputStream(p.getOutputStream());
-			os.writeBytes(cmd + "\n");
-			os.writeBytes("exit\n");
-			os.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public void runRootCommands(String cmd) {
+        Process p = null;
+        try {
+            p = Runtime.getRuntime().exec("su");
+            DataOutputStream os = new DataOutputStream(p.getOutputStream());
+            os.writeBytes(cmd + "\n");
+            os.writeBytes("exit\n");
+            os.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void DisplayToast(String paramString) {
-		Toast.makeText(this, paramString, Toast.LENGTH_SHORT).show();
-	}
+    public void DisplayToast(String paramString) {
+        Toast.makeText(this, paramString, Toast.LENGTH_SHORT).show();
+    }
 }
